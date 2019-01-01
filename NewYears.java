4c60d8a5046c955b395698493af01d39295e1713
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-01-01 00:30:24 -0500

    Update NewYears.java

---
+++ NewYears.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class NewYears 
+{
+	private static int year;
+	static Scanner userInput = new Scanner(System.in);
+	
+	public static void main(String[] args) 
+	{
+		promptForInput();
+		System.out.println("Happy New Years, and have a great " + year + ".");
+	}
+	private static void promptForInput() 
+	{
+		System.out.print("What year is it for you: ");
+		year = userInput.nextInt();
+	}
+}
